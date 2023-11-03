fbcc70c004b05bf84c829c181e6aa715d936f4f3
miuki <56622373+snowmii@users.noreply.github.com>
2023-11-03 20:09:32 +0800

    skip base packages

--- packages.sh
+++ packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pacstrap -i /mnt base base-devel linux linux-lts linux-headers linux-firmware intel-ucode sudo networkmanager dhpcd pulseaudio bluez git
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ~
-yay -S man vim fzf neofetch awesome alacritty nautilus sddm refind xorg-xrandr rofi nvidia nvidia-utils lib32-nvidia-utils nvidia-settings usbutils opentabletdriver google-chrome discord code cava cmatrix extra/zip extra/nerd-fonts extra/adobe-source-han-sans-tw-fonts extra/adobe-source-han-sans-tw-fonts extra/noto-fonts-emoji catppuccin-gtk-theme-macchiato catppuccin-cursors-macchiato 
+yay -S man vim fzf neofetch awesome alacritty fish nautilus sddm refind xorg-xrandr rofi picom nvidia nvidia-utils lib32-nvidia-utils nvidia-settings usbutils opentabletdriver google-chrome discord code cava cmatrix extra/zip extra/nerd-fonts extra/adobe-source-han-sans-tw-fonts extra/adobe-source-han-sans-tw-fonts extra/noto-fonts-emoji catppuccin-gtk-theme-macchiato catppuccin-cursors-macchiato 
